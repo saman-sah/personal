3774b5e0feb67aae322c8efd5ba4487dce6339b8
SamanSah <sahraei.saman91@gmail.com>
2023-03-23 07:48:20 +0430

    remove testimonila and add learning section

---
+++ src/components/home/Learning.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- ======= Services Section ======= -->
+    <section id="learning" class="services">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Learning Videos</h2>
+        </div>
+        <h2>Coming Soon</h2>
+        <div class="row">
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
+            <video controls width="100%" height="auto">
+            </video>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
+            <video controls width="100%" height="auto">
+            </video>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
+            <video controls width="100%" height="auto">
+            </video>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
+            <video controls width="100%" height="auto">
+            </video>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
+            <video controls width="100%" height="auto">
+            </video>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
+            <video controls width="100%" height="auto">
+            </video>
+          </div>
+          
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
